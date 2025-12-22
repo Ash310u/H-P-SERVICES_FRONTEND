70437fc26910768365a7120e4372021e09778058
anindya-mukhopadhyay <youanindya1@gmail.com>
2025-12-22 23:31:28 +0530

    add partner click to open details-with responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,6 +13,7 @@ export default function Footer() {
   return (
     <footer className="footer-section">
       <div className="footer-content">
+        {/* LEFT */}
         <div className="footer-left">
           <div className="footer-logo-row">
             <img src={logo} alt="H&P Logo" className="footer-logo" />
@@ -20,15 +21,51 @@ export default function Footer() {
           </div>
 
           <p className="footer-desc">
-            Ready to transform your vision into reality? Our team is prepared to guide you through every step.
+            Ready to transform your vision into reality? Our team is prepared to
+            guide you through every step.
           </p>
 
+          {/* SOCIAL MEDIA LINKS */}
           <div className="footer-social">
-            <img src={facebookIcon} alt="https://www.facebook.com/share/1Bgz1SJfZx" />
-            <img src={instagramIcon} alt="https://www.instagram.com/hpprojectspvtltd?igsh=NnEyZXY3dGQ0YWhp" />
-            <img src={twitterIcon} alt="twitter / x" />
-            <img src={linkedinIcon} alt="https://www.linkedin.com/company/h-p-projects" />
-            <img src={youtubeIcon} alt="youtube" />
+            <a
+              href="https://www.facebook.com/share/1Bgz1SJfZx"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={facebookIcon} alt="Facebook" />
+            </a>
+
+            <a
+              href="https://www.instagram.com/hpprojectspvtltd?igsh=NnEyZXY3dGQ0YWhp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instagramIcon} alt="Instagram" />
+            </a>
+
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitterIcon} alt="Twitter / X" />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/company/h-p-projects"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinIcon} alt="LinkedIn" />
+            </a>
+
+            <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={youtubeIcon} alt="YouTube" />
+            </a>
           </div>
         </div>
 
@@ -39,19 +76,24 @@ export default function Footer() {
         <div className="footer-right">
           <div>
             <h4>Email</h4>
-            <a href="mailto:info@hpprojects.com.au">info@hpprojects.com.au</a>
+            <a href="mailto:info@hpprojects.com.au">
+              info@hpprojects.com.au
+            </a>
           </div>
 
           <div>
             <h4>Phone</h4>
-            <a href="tel:+918984030821">+918984030821</a>
+            <a href="tel:+918984030821">+91 8984030821</a>
             <br />
-            <a href="tel:+919332627903">+919332627903</a>
+            <a href="tel:+919332627903">+91 9332627903</a>
           </div>
 
           <div>
             <h4>Office</h4>
-            <p>Address: B/28, Gholui,Balichak,Kharagpur, Paschim Medinipur, West Bengal-721124</p>
+            <p>
+              Address: B/28, Gholui, Balichak, Kharagpur, Paschim Medinipur,
+              West Bengal – 721124
+            </p>
           </div>
         </div>
       </div>
